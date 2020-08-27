82714ad5c08ee7817af95d6166e0658ff934589b
moritzploss <moritz.ploss@gmail.com>
2020-08-27 17:55:08 +0200

    Refactoring

--- src/Lib/Parser.hs
+++ src/Lib/Parser.hs
@@ -10,26 +10,38 @@ import qualified Lib.PitchClass as Pitch
 import Lib.Types (Note, PitchClass)
 import qualified Text.Regex.PCRE.Heavy as RegEx
 
-asMatch :: String -> Maybe [String]
+type RegExMatch = [String]
+
+type ChordPattern = String
+
+asMatch :: ChordPattern -> Maybe RegExMatch
 asMatch chord = case RegEx.scan regex chord !! 0 of
   ("", _) -> Nothing
   (_, match) -> Just match
   where
-    regex = [RegEx.re|([A-G]{1})(#|b?)(maj|dom|m?)([6,7,9]?)(?:\/([A-G]))?|]
+    regex = [RegEx.re|([A-G]{1})(#|b?)(M|maj|dim|min|m?)([6,7,9]?)(?:\/([A-G]))?|]
 
-rebase :: String -> String -> Maybe Chord
-rebase root chord = Chord.rebase <$> Pitch.fromString root <*> (parse chord)
+rebase :: String -> ChordPattern -> Maybe Chord
+rebase root pattern = Chord.rebase <$> Pitch.fromString root <*> (parse pattern)
 
-transpose :: Note -> [String] -> Maybe Chord
+transpose :: Note -> RegExMatch -> Maybe Chord
 transpose interval = fmap (Chord.transpose interval) . parse . concat
 
-asChord :: [String] -> Maybe Chord
+asMinorChord :: String -> PitchClass -> Maybe Chord
+asMinorChord added root = case added of
+  "" -> Just $ Cat.minor root
+  "7" -> Just $ Cat.minorSeventh root
+  _ -> Nothing
+
+asChord :: RegExMatch -> Maybe Chord
 asChord matches = case matches of
+  match@[_, _, _, _, root] -> rebase root $ concat $ init match
   (root : "#" : tail) -> transpose 1 (root : tail)
   (root : "b" : tail) -> transpose (-1) (root : tail)
-  match@[_, _, _, _, root] -> rebase root $ concat $ init match
-  [root, _, "7", _] -> Cat.majorSeventh <$> Pitch.fromString root
-  [root, _, _, _] -> Cat.major <$> Pitch.fromString root
+  [root, "", "", _] -> Cat.major <$> Pitch.fromString root
+  [root, "", "maj", _] -> Cat.major <$> Pitch.fromString root
+  [root, "", "m", add] -> Pitch.fromString root >>= asMinorChord add
+  _ -> Nothing
 
-parse :: String -> Maybe Chord
-parse chord = asMatch chord >>= asChord
+parse :: ChordPattern -> Maybe Chord
+parse pattern = asMatch pattern >>= asChord
